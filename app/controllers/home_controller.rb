eb41879062895e72a1f8dae0d6ca8a1a4b78121f
lucascorreaaa <lucascorreasilvaa@gmail.com>
2017-02-03 18:23:01 -0200

    Seleção da Página de onde serão extraídos os dados

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   end
 
   def page_report
+    @page_id = params[:page_id]
     @json = current_user.facebook.get_connection("509293502425881", "?fields=fan_count")
   end
 end

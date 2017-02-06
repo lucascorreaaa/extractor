91d538df054cfc0a4c37e9df07e3565c0a28c225
lucascorreaaa <lucascorreasilvaa@gmail.com>
2017-02-06 15:50:24 -0200

    Exportando JSON em CSV - Query temporária

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class HomeController < ApplicationController
   def index
   	@dados = "Lucas Correa Silva"
@@ -5,6 +7,14 @@ class HomeController < ApplicationController
 
   def page_report
     @page_id = params[:page_id]
-    @json = current_user.facebook.get_connection(@page_id, "?fields=fan_count,name")
+    @json = current_user.facebook.get_connection(1219532218116699, "/feed?fields=message, updated_time, id&limit=9999999")
+    @csv_string = CSV.generate do |csv|
+      csv << ["message","updated_time","id"]
+      @json.each do |hash|
+        csv << [hash["message"], hash["updated_time"], hash["id"]]
+      end
+    end
+    send_data @csv_string, type: 'text/csv; charset=utf-8; header=present', disposition: "attachment; filename=page_report.csv"
+    #@json = current_user.facebook.get_connection(@page_id, "?fields=fan_count,name")
   end
 end
